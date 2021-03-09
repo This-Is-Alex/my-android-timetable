dfbedfcc147578e068c5c907a0ca069b29d49a51
Alex Hobson <aho113@uclive.ac.nz>
2021-03-09 20:33:28 +1300

    Created navigation menu

--- app/src/main/java/me/ahobson/myandroidtimetable/MainActivity.kt
+++ app/src/main/java/me/ahobson/myandroidtimetable/MainActivity.kt
@@ -2,10 +2,19 @@ package me.ahobson.myandroidtimetable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
 }
